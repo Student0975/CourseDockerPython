d5a681814debfdc5139e7680b67273f65b415801
Student0975 <student0975@gmail.com>
2024-08-04 18:39:33 +0100

    fix: added a new await message.send_copy

--- bot/src/handlers.py
+++ bot/src/handlers.py
@@ -43,6 +43,7 @@ async def echo_handler(message: Message) -> None:
         insert_data(message)
         await message.send_copy(chat_id=message.chat.id)
         await message.send_copy(chat_id=message.chat.id)
+        await message.send_copy(chat_id=message.chat.id)
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
