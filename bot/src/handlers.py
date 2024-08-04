a725e6cc9393bf41fd04b298db703cdd18752c1d
Student0975 <student0975@gmail.com>
2024-08-04 18:35:01 +0100

    fix: deleted redundant await.message

--- bot/src/handlers.py
+++ bot/src/handlers.py
@@ -43,7 +43,6 @@ async def echo_handler(message: Message) -> None:
         insert_data(message)
         await message.send_copy(chat_id=message.chat.id)
         await message.send_copy(chat_id=message.chat.id)
-        await message.send_copy(chat_id=message.chat.id)
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
