6b71090255561c243f15d60dd7b51224474bd335
Student0975 <student0975@gmail.com>
2024-11-26 13:34:18 +0000

    minor changes

--- bot/src/handlers.py
+++ bot/src/handlers.py
@@ -46,8 +46,8 @@ async def echo_handler(message: Message) -> None:
         insert_data(message)
         await message.send_copy(chat_id=message.chat.id)
         await message.answer("Bonnie Scotland! Bon Accord!" +
-                             "Alba Since and Forever!" +
-                             "Learn this Motto by heart!")
+                             " Alba Since and Forever!" +
+                             " Learn this Motto by heart!")
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
